620a82466ea005b49e326c7a8c150b07e28a2d7d
Elliot Winkler <elliot.winkler@gmail.com>
2014-05-26 22:02:58 -0600

    Use flexbox to align/size fields more freely

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -15,6 +15,16 @@ SimpleForm.setup do |config|
     end
   end
 
+  config.wrappers :vertical_input_group, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label, class: 'control-label'
+
+    b.use :input, class: 'form-control', wrap_with: { tag: 'div', class: 'input-group' }
+    b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+  end
+
   config.wrappers :vertical_file_input, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
